35e622a79d90c7e502674d2f633ffa3aec3e812e
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-06-06 19:47:50 -0300

    Adicionado Refit

--- src/web/NSE.WebApp.MVC/Services/ICatalogoService.cs
+++ src/web/NSE.WebApp.MVC/Services/ICatalogoService.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using NSE.WebApp.MVC.Models;
+using Refit;
 
 namespace NSE.WebApp.MVC.Services
 {
     public interface ICatalogoService
     {
+        [Get("/catalogo/produtos/")]
         Task<IEnumerable<ProdutoViewModel>> ObterTodos();
+
+        [Get("/catalogo/produtos/{id}")]
         Task<ProdutoViewModel> ObterPorId(Guid id);
     }
 }
