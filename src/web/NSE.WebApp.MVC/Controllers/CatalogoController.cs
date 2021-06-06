2f5867521d474eef09f8da2f2a9d01ec381f2d47
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-06-06 18:17:03 -0300

    Criado CatalogoController é telas

---
+++ src/web/NSE.WebApp.MVC/Controllers/CatalogoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    public class CatalogoController : Controller
+    {
+        [HttpGet]
+        [Route("")]
+        [Route("vitrine")]
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("produto-detalhe/{id}")]
+        public async Task<IActionResult> ProdutoDetalhe(Guid id)
+        {
+            return View();
+        }
+    }
+}
