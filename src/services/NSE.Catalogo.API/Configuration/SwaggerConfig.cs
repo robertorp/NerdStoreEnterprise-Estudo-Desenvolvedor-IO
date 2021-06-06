9f26c5e06f462bfab758129c19c479f05e791b65
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-06-06 18:04:22 -0300

    Adicionado autenticação por swagger

--- src/services/NSE.Catalogo.API/Configuration/SwaggerConfig.cs
+++ src/services/NSE.Catalogo.API/Configuration/SwaggerConfig.cs
@@ -18,6 +18,31 @@ namespace NSE.Catalogo.API.Configuration
                     Contact = new OpenApiContact { Name = "Roberto Alves Pereira", Email = "robertoalvespereira18@gmail.com" },
                     License = new OpenApiLicense { Name = "MIT", Url = new Uri("https://opensource.org/licenses/mit") }
                 });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insira o token JWT desta maneira: Bearer {seu token}",
+                    Name = "Authorization",
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
             });
         }
 
