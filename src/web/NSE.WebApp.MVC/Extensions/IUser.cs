bf87b8f839aaa16c21c81e1ae0de89c2da968bd3
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-06-05 21:03:29 -0300

    Obtendo usuário do aspnet

---
+++ src/web/NSE.WebApp.MVC/Extensions/IUser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+
+namespace NSE.WebApp.MVC.Extensions
+{
+    public interface IUser
+    {
+        string Name { get; }
+        Guid ObterUserId();
+        string ObterUserEmail();
+        string ObterUserToken();
+        bool EstaAutenticado();
+        bool PossuiRole(string role);
+        IEnumerable<Claim> ObterClaims();
+        HttpContext ObterHttpContext();
+    }
+}
