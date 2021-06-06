e1df55e661d6c0da818121daaf81c86f48d40c07
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-06-06 13:51:35 -0300

    Criado tabela Produtos

---
+++ src/services/NSE.Catalogo.API/Data/CatalogoContext.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using NSE.Catalogo.API.Models;
+
+namespace NSE.Catalogo.API.Data
+{
+    public class CatalogoContext : DbContext
+    {
+        public CatalogoContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var mutableProperty in modelBuilder.Model.GetEntityTypes().SelectMany(
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+            {
+                mutableProperty.SetColumnType("varchar(100)");
+            }
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CatalogoContext).Assembly);
+        }
+    }
+}
---
+++ src/services/NSE.Catalogo.API/Migrations/20210606165006_Initial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NSE.Catalogo.API.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produtos",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Nome = table.Column<string>(type: "varchar(250)", nullable: false),
+                    Descricao = table.Column<string>(type: "varchar(500)", nullable: false),
+                    Ativo = table.Column<bool>(type: "boolean", nullable: false),
+                    Valor = table.Column<decimal>(type: "numeric", nullable: false),
+                    DataCadastro = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Imagem = table.Column<string>(type: "varchar(250)", nullable: false),
+                    QuantidadeEstoque = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produtos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produtos");
+        }
+    }
+}
