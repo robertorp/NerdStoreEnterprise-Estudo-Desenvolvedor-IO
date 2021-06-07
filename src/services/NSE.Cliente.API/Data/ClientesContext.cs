ccc734124ae0a8e8ad0ae92da810a15575bfd45f
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-06-07 09:01:40 -0300

    Criado mapeamento cliente é endereco

--- src/services/NSE.Cliente.API/Data/ClientesContext.cs
+++ src/services/NSE.Cliente.API/Data/ClientesContext.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using NSE.Cliente.API.Models;
 using NSE.Core.Data;
 
 namespace NSE.Cliente.API.Data
@@ -8,6 +10,23 @@ namespace NSE.Cliente.API.Data
     {
         public ClientesContext(DbContextOptions options) : base(options)
         {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+
+        public DbSet<Models.Cliente> Clientes { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            foreach (var property in modelBuilder.Model.GetEntityTypes().SelectMany(
+                e => e.GetProperties().Where(p => p.ClrType == typeof(string))))
+                property.SetColumnType("varchar(100)");
+
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(e => e.GetForeignKeys())) relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(ClientesContext).Assembly);
         }
 
         public async Task<bool> Commit()
