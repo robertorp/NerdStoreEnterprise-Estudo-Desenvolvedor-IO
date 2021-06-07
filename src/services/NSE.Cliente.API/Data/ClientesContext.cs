bec124b97eb4db4f607ee94f87cee8e36cad4b45
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-06-07 06:03:24 -0300

    Criado Projeto Inicial para Clientes

---
+++ src/services/NSE.Cliente.API/Data/ClientesContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NSE.Core.Data;
+
+namespace NSE.Cliente.API.Data
+{
+    public class ClientesContext : DbContext, IUnitOfWork
+    {
+        public ClientesContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public async Task<bool> Commit()
+        {
+            return await base.SaveChangesAsync() > 0;
+        }
+    }
+}
