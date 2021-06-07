e23fedb5d818c148f926047301f650b0ca8d8d2e
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-06-07 09:07:59 -0300

    Criando estrutura command

---
+++ src/services/NSE.Cliente.API/Application/Commands/RegistrarClienteCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NSE.Cliente.API.Application.Commands
+{
+    public class RegistrarClienteCommand
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Cpf { get; set; }
+    }
+}
