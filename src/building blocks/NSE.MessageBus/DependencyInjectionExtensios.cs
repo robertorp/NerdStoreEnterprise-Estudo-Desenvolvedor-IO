b6dd104fc0835439ad883cf3d015135296e3b99d
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-06-08 14:21:29 -0300

    Criando dependency Injection para MessageBus nosso

---
+++ src/building blocks/NSE.MessageBus/DependencyInjectionExtensios.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NSE.MessageBus
+{
+    public static class DependencyInjectionExtensios
+    {
+        public static IServiceCollection AddMessageBus(this IServiceCollection services, string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString)) throw new ArgumentException();
+
+            services.AddSingleton<IMessageBus>(new MessageBus(connectionString));
+
+            return services;
+        }
+    }
+}
