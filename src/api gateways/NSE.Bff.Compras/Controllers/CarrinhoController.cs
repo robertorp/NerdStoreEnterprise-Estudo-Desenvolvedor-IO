7455d17490d356941e5790959ea32ae9f99b8d15
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-06-11 09:59:36 -0300

    Adicionado controler

---
+++ src/api gateways/NSE.Bff.Compras/Controllers/CarrinhoController.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NSE.WebAPI.Core.Controller;
+
+namespace NSE.Bff.Compras.Controllers
+{
+    public class CarrinhoController : MainController
+    {
+        [HttpGet]
+        [Route("compras/carrinho")]
+        public async Task<IActionResult> Index()
+        {
+            return CustomResponse();
+        }
+
+        [HttpGet]
+        [Route("compras/carrinho-quantidade")]
+        public async Task<IActionResult> ObterQuantidadeCarrinho()
+        {
+            return CustomResponse();
+        }
+
+        [HttpPost]
+        [Route("compras/carrinho/items")]
+        public async Task<IActionResult> AdicionarItemCarrinho()
+        {
+            return CustomResponse();
+        }
+
+        [HttpPut]
+        [Route("compras/carrinho/items/{produtoId}")]
+        public async Task<IActionResult> AtualizarItemCarrinho()
+        {
+            return CustomResponse();
+        }
+
+        [HttpDelete]
+        [Route("compras/carrinho/items/{produtoId}")]
+        public async Task<IActionResult> RemoverItemCarrinho()
+        {
+            return CustomResponse();
+        }
+    }
+}
