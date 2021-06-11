a29b907f4cfcb66d4294d18f4f57671b2bd827ab
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-06-10 21:04:41 -0300

    ajustes

--- src/web/NSE.WebApp.MVC/Models/ErrorViewModel.cs
+++ src/web/NSE.WebApp.MVC/Models/ErrorViewModel.cs
@@ -12,6 +12,11 @@ namespace NSE.WebApp.MVC.Models
 
     public class ResponseResult
     {
+        public ResponseResult()
+        {
+            Errors = new ResponseErrorMessages();
+        }
+
         public string Title { get; set; }
         public int Status { get; set; }
         public ResponseErrorMessages Errors { get; set; }
@@ -19,6 +24,11 @@ namespace NSE.WebApp.MVC.Models
 
     public class ResponseErrorMessages
     {
+        public ResponseErrorMessages()
+        {
+            Mensagens = new List<string>();
+        }
+
         public List<string> Mensagens { get; set; }
     }
 }
