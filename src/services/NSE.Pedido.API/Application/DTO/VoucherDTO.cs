dcb9dc0d83a2854d9a860d3b6b515b6568d82070
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-06-24 21:27:31 -0300

    VoucherDTO Criado

--- src/services/NSE.Pedido.API/Application/DTO/VoucherDTO.cs
+++ src/services/NSE.Pedido.API/Application/DTO/VoucherDTO.cs
@@ -2,6 +2,9 @@
 {
     public class VoucherDTO
     {
-        
+        public string Codigo { get; set; }
+        public decimal? Percentual { get; set; }
+        public decimal? ValorDesconto { get; set; }
+        public int TipoDesconto { get; set; }
     }
 }
