789b8981650877271da468ee4e2b3938087fa1a1
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-06-24 21:35:19 -0300

    Criando query voucher

---
+++ src/services/NSE.Pedido.API/Application/Queries/VoucherQueries.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading.Tasks;
+using NSE.Pedidos.API.Application.DTO;
+using NSE.Pedidos.Domain.Vouchers;
+
+namespace NSE.Pedidos.API.Application.Queries
+{
+    public interface IVoucherQueries
+    {
+        Task<VoucherDTO> ObterVoucherPorCodigo(string codigo);
+    }
+
+    public class VoucherQueries : IVoucherQueries
+    {
+        private IVoucherRepository _voucherRepository;
+
+        public VoucherQueries(IVoucherRepository voucherRepository)
+        {
+            _voucherRepository = voucherRepository;
+        }
+
+        public async Task<VoucherDTO> ObterVoucherPorCodigo(string codigo)
+        {
+            var voucher = await _voucherRepository.ObterVoucherPorCodigo(codigo);
+
+            if (voucher == null) return null;
+
+            return new VoucherDTO
+            {
+                Codigo = voucher.Codigo,
+                Percentual = voucher.Percentual,
+                TipoDesconto = (int) voucher.TipoDesconto,
+                ValorDesconto = voucher.ValorDesconto
+            };
+        }
+    }
+}
--- src/services/NSE.Pedido.API/Controllers/VoucherController.cs
+++ src/services/NSE.Pedido.API/Controllers/VoucherController.cs
@@ -1,9 +1,15 @@
-﻿using NSE.WebAPI.Core.Controller;
+﻿using Microsoft.AspNetCore.Mvc;
+using NSE.Pedidos.API.Application.DTO;
+using NSE.WebAPI.Core.Controller;
 
 namespace NSE.Pedidos.API.Controllers
 {
     public class VoucherController : MainController
     {
-        
+        [HttpGet]
+        public IActionResult ObterVaucher()
+        {
+            return CustomResponse(new VoucherDTO());
+        }
     }
 }
