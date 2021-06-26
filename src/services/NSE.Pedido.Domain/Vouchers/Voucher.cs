5b158f128c2cc4117073f3e30ca7419a6ca72ed8
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-06-26 17:26:13 -0300

    Implementando processo de salvar pedido

--- src/services/NSE.Pedido.Domain/Vouchers/Voucher.cs
+++ src/services/NSE.Pedido.Domain/Vouchers/Voucher.cs
@@ -31,5 +31,13 @@ namespace NSE.Pedidos.Domain.Vouchers
             Utilizado = true;
             Quantidade = 0;
         }
+
+        public void DebitarQuantidade()
+        {
+            Quantidade -= 1;
+            if (Quantidade >= 1) return;
+
+            MarcarComoUtilizado();
+        }
     }
 }
