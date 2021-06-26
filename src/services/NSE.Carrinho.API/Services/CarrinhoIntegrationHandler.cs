4b236437d5adecb5eeb3e38068160f57a403a94b
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-06-26 20:13:15 -0300

    Adicionado Migração Carrinho

---
+++ src/services/NSE.Carrinho.API/Services/CarrinhoIntegrationHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NSE.Carrinho.API.Data;
+using NSE.Core.Messages.Integration;
+using NSE.MessageBus;
+
+namespace NSE.Carrinho.API.Services
+{
+    public class CarrinhoIntegrationHandler : BackgroundService
+    {
+        private readonly IMessageBus _bus;
+        private readonly IServiceProvider _serviceProvider;
+
+        public CarrinhoIntegrationHandler(IMessageBus bus, IServiceProvider serviceProvider)
+        {
+            _bus = bus;
+            _serviceProvider = serviceProvider;
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            SetSubscribes();
+            return Task.CompletedTask;
+        }
+
+        private void SetSubscribes()
+        {
+            _bus.SubscribeAsync<PedidoRealizadoIntegrationEvent>("PedidoRealizado", async request => 
+                await ApagarCarrinho(request));
+        }
+
+        private async Task ApagarCarrinho(PedidoRealizadoIntegrationEvent message)
+        {
+            using var scope = _serviceProvider.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<CarrinhoContext>();
+
+            var carrinho = await context.CarrinhoCliente.FirstOrDefaultAsync(c => c.ClienteId == message.ClienteId);
+
+            if (carrinho != null)
+            {
+                context.CarrinhoCliente.Remove(carrinho);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
--- src/services/NSE.Pedido.API/Configuration/MessageBusConfig.cs
+++ src/services/NSE.Pedido.API/Configuration/MessageBusConfig.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NSE.Core.Utils;
+using NSE.MessageBus;
 
 namespace NSE.Pedidos.API.Configuration
 {
@@ -8,7 +10,7 @@ namespace NSE.Pedidos.API.Configuration
         public static void AddMessageBusConfiguration(this IServiceCollection services,
             IConfiguration configuration)
         {
-
+            services.AddMessageBus(configuration.GetMessageQueueConnection("MessageBus"));
         }
     }
 }
