ec3198479cb8dfef470485ea65ea9eab9bddf8ac
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-06-25 22:30:25 -0300

    Pedido

--- src/services/NSE.Pedido.Domain/Pedidos/IPedidoRepository.cs
+++ src/services/NSE.Pedido.Domain/Pedidos/IPedidoRepository.cs
@@ -1,7 +1,21 @@
-﻿namespace NSE.Pedidos.Domain.Pedidos
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NSE.Core.Data;
+
+namespace NSE.Pedidos.Domain.Pedidos
 {
-    public interface IPedidoRepository
+    public interface IPedidoRepository : IRepository<Pedido>
     {
-        
+        Task<Pedido> ObterPorId(Guid id);
+        Task<IEnumerable<Pedido>> ObterListaPorClienteId(Guid clienteId);
+        void Adicionar(Pedido pedido);
+        void Atualizar(Pedido pedido);
+
+
+
+        // Pedido Item
+        Task<PedidoItem> ObterItemPorId(Guid id);
+        Task<PedidoItem> ObterItemPorPedido(Guid pedidoId, Guid produtoId);
     }
 }
--- src/services/NSE.Pedido.Domain/Pedidos/PedidoStatus.cs
+++ src/services/NSE.Pedido.Domain/Pedidos/PedidoStatus.cs
@@ -2,6 +2,10 @@
 {
     public enum PedidoStatus
     {
-        
+        Autorizado = 1,
+        Pago = 2,
+        Recusado = 3,
+        Entregue = 4,
+        Cancelado = 5
     }
 }
