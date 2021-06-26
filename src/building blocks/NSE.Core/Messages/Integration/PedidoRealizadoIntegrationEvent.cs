22d62976539a3d9190aeb329bd92d074306e8c08
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-06-26 19:10:10 -0300

    Adicionado Classe de Integração de Pedido com Carrinho publicado evento de Pedido para o MessageBus , o Carrinho que vai receber

---
+++ src/building blocks/NSE.Core/Messages/Integration/PedidoRealizadoIntegrationEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NSE.Core.Messages.Integration
+{
+    public class PedidoRealizadoIntegrationEvent : IntegrationEvent
+    {
+        public Guid ClienteId { get; set; }
+
+        public PedidoRealizadoIntegrationEvent(Guid clienteId)
+        {
+            ClienteId = clienteId;
+        }
+    }
+}
---
+++ src/services/NSE.Pedido.API/Application/Events/PedidoEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using NSE.Core.Messages.Integration;
+using NSE.MessageBus;
+
+namespace NSE.Pedidos.API.Application.Events
+{
+    public class PedidoEventHandler : INotificationHandler<PedidoRealizadoEvent>
+    {
+        private readonly IMessageBus _messageBus;
+
+        public PedidoEventHandler(IMessageBus messageBus)
+        {
+            _messageBus = messageBus;
+        }
+
+        public async Task Handle(PedidoRealizadoEvent menssage, CancellationToken cancellationToken)
+        {
+            await _messageBus.PublishAsync(new PedidoRealizadoIntegrationEvent(menssage.ClienteId));
+        }
+    }
+}
