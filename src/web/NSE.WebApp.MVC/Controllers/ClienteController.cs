7b716e1ebd187e7e5141f7d472ad070a0e31aba3
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-06-27 15:02:11 -0300

    Adicionado ClienteController

---
+++ src/web/NSE.WebApp.MVC/Controllers/ClienteController.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    [Authorize]
+    public class ClienteController : MainController
+    {
+        private readonly IClienteService _clienteService;
+
+        public ClienteController(IClienteService clienteService)
+        {
+            _clienteService = clienteService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> NovoEndereco(EnderecoViewModel endereco)
+        {
+            var response = await _clienteService.AdicionarEndereco(endereco);
+
+            if (ResponsePossuiErros(response)) TempData["Erros"] =
+                ModelState.Values.SelectMany(v => v.Errors.Select(e => e.ErrorMessage)).ToList();
+
+            return RedirectToAction("EnderecoEntrega", "Pedido");
+        }
+    }
+}
