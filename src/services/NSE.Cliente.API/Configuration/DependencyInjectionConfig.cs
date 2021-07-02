340f5293625e7fe07cce66ec84ebe9d612aead40
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-07-01 22:58:42 -0300

    cliente

--- src/services/NSE.Cliente.API/Configuration/DependencyInjectionConfig.cs
+++ src/services/NSE.Cliente.API/Configuration/DependencyInjectionConfig.cs
@@ -1,12 +1,13 @@
 ﻿using FluentValidation.Results;
 using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using NSE.Cliente.API.Application.Commands;
 using NSE.Cliente.API.Application.Events;
 using NSE.Cliente.API.Data;
 using NSE.Cliente.API.Models;
-using NSE.Cliente.API.Services;
 using NSE.Core.Mediator;
+using NSE.WebAPI.Core.Usuario;
 
 namespace NSE.Cliente.API.Configuration
 {
@@ -14,6 +15,8 @@ namespace NSE.Cliente.API.Configuration
     {
         public static void RegisterServices(this IServiceCollection services)
         {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IAspNetUser, AspNetUser>();
             services.AddScoped<IMediatorHandler, MediatorHandler>();
             services.AddScoped<IRequestHandler<RegistrarClienteCommand, ValidationResult>, ClienteCommandHandler>();
             services.AddScoped<IRequestHandler<AdicionarEnderecoCommand, ValidationResult>, ClienteCommandHandler>();
