3b70cf16df4197e14aa5637b96329a1e9e4c3906
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-07-01 21:51:00 -0300

    ajustes

---
+++ src/api gateways/NSE.Bff.Compras/Services/IClienteService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using NSE.Bff.Compras.Models;
+
+namespace NSE.Bff.Compras.Services
+{
+    public interface IClienteService
+    {
+        Task<EnderecoDTO> ObterEndereco();
+    }
+}
