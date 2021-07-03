b234dc3ae6ca08735ef53150df7724b63ebafd4e
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-07-03 09:40:40 -0300

    Injetando PagamentoConfig via IOptions

--- src/services/NSE.Pagamento.API/Facade/PagamentoCartaoCreditoFacade.cs
+++ src/services/NSE.Pagamento.API/Facade/PagamentoCartaoCreditoFacade.cs
@@ -1,7 +1,14 @@
-﻿namespace NSE.Pagamento.API.Facade
+﻿using Microsoft.Extensions.Options;
+
+namespace NSE.Pagamento.API.Facade
 {
     public class PagamentoCartaoCreditoFacade : IPagamentoFacade
     {
-        
+        private readonly PagamentoConfig _pagamentoConfig;
+
+        public PagamentoCartaoCreditoFacade(IOptions<PagamentoConfig> pagamentoConfig)
+        {
+            _pagamentoConfig = pagamentoConfig.Value;
+        }
     }
 }
