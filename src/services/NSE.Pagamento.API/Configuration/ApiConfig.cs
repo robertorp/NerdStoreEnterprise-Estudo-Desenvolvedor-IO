5d7c2446ece449718eef9a529ba865e71b70ff1e
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-07-02 22:21:10 -0300

    configurando base de pagamento api

--- src/services/NSE.Pagamento.API/Configuration/ApiConfig.cs
+++ src/services/NSE.Pagamento.API/Configuration/ApiConfig.cs
@@ -1,21 +1,16 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using NSE.Pedidos.Infra.Data;
 using NSE.WebAPI.Core.Identidade;
 
-namespace NSE.Pedidos.API.Configuration
+namespace NSE.Pagamento.API.Configuration
 {
     public static class ApiConfig
     {
         public static void AddApiConfiguration(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddDbContext<PedidosContext>(options =>
-                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
-
             services.AddControllers();
 
             services.AddCors(options =>
---
+++ src/services/NSE.Pagamento.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using NSE.WebAPI.Core.Usuario;
+
+namespace NSE.Pagamento.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void RegisterServices(this IServiceCollection services)
+        {
+            // API
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IAspNetUser, AspNetUser>();
+        }
+    }
+}
