d65cb7d51cb333e0e71038c32369b08ea97f1dca
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-07-03 09:25:05 -0300

    pagamentos salvando dados

---
+++ src/services/NSE.Pagamento.API/Data/Mappings/PagamentoMapping.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace NSE.Pagamento.API.Data.Mappings
+{
+    public class PagamentoMapping : IEntityTypeConfiguration<Pagamento.API.Models.Pagamento>
+    {
+        public void Configure(EntityTypeBuilder<Pagamento.API.Models.Pagamento> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Ignore(c => c.CartaoCredito);
+
+            // 1 : N => Pagamento : Transacao
+            builder.HasMany(c => c.Transacoes)
+                .WithOne(c => c.Pagamento)
+                .HasForeignKey(c => c.PagamentoId);
+
+            builder.ToTable("Pagamentos");
+        }
+    }
+}
