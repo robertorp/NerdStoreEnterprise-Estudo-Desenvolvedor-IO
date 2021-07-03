bec6461b2aec165c7c4947f2645bd1b759c1782f
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-07-03 09:18:27 -0300

    Models

---
+++ src/services/NSE.Pagamento.API/Models/IPagamentoRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using NSE.Core.Data;
+
+namespace NSE.Pagamento.API.Models
+{
+    public interface IPagamentoRepository : IRepository<Pagamento>
+    {
+        void AdicionarPagamento(NSE.Pagamento.API.Models.Pagamento pagamento);
+        void AdicionarTransacao(Transacao transacao);
+        Task<NSE.Pagamento.API.Models.Pagamento> ObterPagamentoPorPedidoId(Guid pedidoId);
+        Task<IEnumerable<Transacao>> ObterTransacaoesPorPedidoId(Guid pedidoId);
+    }
+}
