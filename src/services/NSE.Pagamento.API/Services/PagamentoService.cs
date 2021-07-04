55fe67c6dce22cbb792adfb89b9e29880d1b3f0e
ROBERTO ALVES PEREIRA <robertoalvespereira18@gmail.com>
2021-07-03 21:18:22 -0300

    Implementado PagamentoService

--- src/services/NSE.Pagamento.API/Services/PagamentoService.cs
+++ src/services/NSE.Pagamento.API/Services/PagamentoService.cs
@@ -1,13 +1,50 @@
 ﻿using System.Threading.Tasks;
+using FluentValidation.Results;
 using NSE.Core.Messages.Integration;
+using NSE.Pagamento.API.Facade;
+using NSE.Pagamento.API.Models;
 
 namespace NSE.Pagamento.API.Services
 {
     public class PagamentoService : IPagamentoService
     {
-        public Task<ResponseMessage> AutorizarPagamento(Models.Pagamento pagamento)
+        private readonly IPagamentoFacade _pagamentoFacade;
+        private readonly IPagamentoRepository _pagamentoRepository;
+
+        public PagamentoService(IPagamentoFacade pagamentoFacade, 
+                                IPagamentoRepository pagamentoRepository)
         {
-            throw new System.NotImplementedException();
+            _pagamentoFacade = pagamentoFacade;
+            _pagamentoRepository = pagamentoRepository;
+        }
+
+        public async Task<ResponseMessage> AutorizarPagamento(Models.Pagamento pagamento)
+        {
+            var transacao = await _pagamentoFacade.AutorizarPagamento(pagamento);
+            var validationResult = new ValidationResult();
+
+            if (transacao.Status != StatusTransacao.Autorizado)
+            {
+                validationResult.Errors.Add(new ValidationFailure("Pagamento", 
+                    "Pagamento recusado, entre em contato com a sua operadora de cartão"));
+
+                return new ResponseMessage(validationResult);
+            }
+
+            pagamento.AdicionarTransacao(transacao);
+            _pagamentoRepository.AdicionarPagamento(pagamento);
+
+            if (!await _pagamentoRepository.UnitOfWork.Commit())
+            {
+                validationResult.Errors.Add(new ValidationFailure("Pagamento", 
+                    "Houve um erro ao realizar o estorno."));
+
+                // TODO: Comunicar com o gateway para realizar o estorno.
+
+                return new ResponseMessage(validationResult);
+            }
+
+            return new ResponseMessage(validationResult);
         }
     }
 }
